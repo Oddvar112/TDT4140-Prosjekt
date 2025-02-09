d40bcbe75f189da5eb2068a2ecb14cb8940a84e7
BuildTools <unconfigured@null.spigotmc.org>
2025-02-09 19:30:40 +0100

    feat(events): La til EventCard-komponent for visning av arrangementer.

---
+++ appevent-frontend/app/components/events/styles/EventCardStyles.js
@@ -0,0 +1,32 @@
+export const cardStyles = {
+  width: 345,
+  height: 310,
+  display: "flex",
+  flexDirection: "column",
+};
+
+export const cardActionStyles = {
+  flexGrow: 1,
+  overflow: "hidden",
+  ":hover": { overflowY: "auto" },
+};
+
+export const iconBoxStyles = {
+  display: "flex",
+  alignItems: "center",
+  color: "text.secondary",
+};
+
+export const buttonStyles = {
+  margin: 1,
+};
+
+export const titleStyles = {
+  mb: 1,
+  overflowWrap: "break-word",
+};
+
+export const descriptionStyles = {
+  mt: 1.2,
+  color: "text.secondary",
+};
