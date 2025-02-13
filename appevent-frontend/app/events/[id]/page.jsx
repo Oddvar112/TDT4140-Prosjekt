1381df5648bdf788488e435d56203e84f1417a61
Ba Huy Pham <bahp@stud.ntnu.no>
2025-02-13 01:21:00 +0100

    ref(evnets): Gjorde om komponenter og nå blir shadcn brukt istendenfor material UI, smat bruker de nye API filene

---
+++ appevent-frontend/app/events/[id]/page.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { useParams } from "next/navigation";
+import { SingleEventFetch } from "../../../api/events/getEvent";
+import { EventToggleButton } from "../../../components/event/EventToggleButton";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Calendar, MapPin, Users, ChevronLeft } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function EventDetail() {
+  const params = useParams();
+  const { event, isLoading, error, refresh } = SingleEventFetch(params.id);
+  const router = useRouter();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  if (!event) return <div>No event found</div>;
+
+  return (
+    <div className="max-w-3xl mx-auto space-y-8 animate-in my-16">
+      <div className="flex items-start -mb-6">
+        <Button
+          variant="ghost"
+          onClick={() => router.back()}
+          className="hover:bg-gray-100 rounded-full p-2"
+        >
+          <ChevronLeft className="h-6 w-6" />
+          <span className="sr-only">Back</span>
+        </Button>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-3xl">{event.title}</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <div className="space-y-2">
+            <div className="flex items-center text-muted-foreground">
+              <Calendar className="mr-2 h-5 w-5" />
+              {new Date(event.dateTime).toLocaleString()}
+            </div>
+            <div className="flex items-center text-muted-foreground">
+              <MapPin className="mr-2 h-5 w-5" />
+              {event.location}
+            </div>
+            <div className="flex items-center text-muted-foreground">
+              <Users className="mr-2 h-5 w-5" />
+              {event.participants.length || 0} påmeldte
+            </div>
+          </div>
+
+          <div className="space-y-2">
+            <h3 className="font-semibold">Om arrangementet</h3>
+            <p className="text-muted-foreground">{event.description}</p>
+          </div>
+
+          <EventToggleButton activityId={event.id} onToggleSuccess={refresh} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
