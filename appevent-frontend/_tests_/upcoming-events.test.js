b5469ce427e75e0b411dc10d2acf58032b0c5844
Ba Huy <bahp@stud.ntnu.no>
2025-02-17 13:34:11 +0100

    fix(test): Oppdaterte test for riktig datoformat

--- appevent-frontend/_tests_/upcoming-events.test.js
+++ appevent-frontend/_tests_/upcoming-events.test.js
@@ -98,7 +98,13 @@ describe('UpcomingEventPage', () => {
     render(<UpcomingEventPage />)
     
     mockEvents.forEach(event => {
-      const formattedDate = new Date(event.dateTime).toLocaleString()
+      const formattedDate = new Date(event.dateTime).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      })
       expect(screen.getByText(formattedDate)).toBeInTheDocument()
     })
   })
